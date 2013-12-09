a485298dc156341c4b8a98a23c27251b81baa8a8
Tomas Krejci <krejcit1@u-pl20.ms.mff.cuni.cz>
2013-12-09 15:07:54 +0100

    initial commit

---
+++ src/Buddha.h
@@ -0,0 +1,52 @@
+#ifndef _BUDDHA_H
+#define _BUDDHA_H
+
+#include <complex>
+#include <cstdint>
+#include <exception>
+#include <mutex>
+#include <thread>
+#include <vector>
+
+#include "CImg.h"
+
+using namespace cimg_library;
+
+class MaxIterationsTooBig : public virtual std::exception { };
+
+class Buddha {
+public:
+	typedef double floating_type;
+	typedef	std::complex<floating_type> complex_type; 
+
+	Buddha(const Params & p, const std::size_t thread_vector_size = 10 * 1024 * 1024);
+
+	struct Params {
+		uint64_t width;
+		floating_type radius;
+		uint64_t max_iterations;
+		int num_threads;
+	};
+
+	static Params get_empty_params();
+
+	void run();
+private:
+	uint64_t x_size_;
+	uint64_t y_size_;
+	std::vector<uint64_t> data_;
+	std::mutex data_lock_;
+
+	std::pair<uint64_t, uint64_t> lin2car(uint64_t pos) const;
+	uint64_t car2lin(uint64_t x, uint64_t y) const;
+
+	complex_type car2complex(uint64_t x, uint64_t y) const;
+	std::pair<uint64_t, uint64_t> complex2car(complex_type c) const;
+
+	const std::size_t thread_vector_size_;
+	std::size_t num_threads_;
+	std::vector<std::thread> threads_;
+	void worker(uint64_t from, uint64_t to);
+};
+
+#endif // _BUDDHA_H
---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(int argc, char * argv[]) {
+
+	try {
+		auto params = Buddha::get_empty_params();
+		params.width = 512;
+		params.radius = 2;
+		params.max_iterations = 20;
+
+		Buddha b(params);
+		b.run();
+	} catch (std::exception & e) {
+		std::cerr << "EXCEPTION : " << e.what() << std::endl;
+	} catch (...) {
+		std::cerr << "UNKNOWN EXCEPTION" << std::endl;
+	}
+
+	return 0;
+}
