bc1a606f251bf5d300f90602bb559d9f2981558b
Tomas Krejci <tomas789@gmail.com>
2013-12-09 16:21:11 +0100

    tabs replaced

--- src/Buddha.h
+++ src/Buddha.h
@@ -13,42 +13,42 @@ class MaxIterationsTooBigException : public virtual std::exception { };
 
 class Buddha {
 public:
-	typedef double floating_type;
-	typedef	std::complex<floating_type> complex_type; 
+    typedef double floating_type;
+    typedef std::complex<floating_type> complex_type; 
 
-	struct Params {
-		uint64_t width;
-		floating_type radius;
-		uint64_t max_iterations;
-		int num_threads;
-	};
+    struct Params {
+        uint64_t width;
+        floating_type radius;
+        uint64_t max_iterations;
+        int num_threads;
+    };
 
-	Buddha(const Params & p, const std::size_t thread_vector_size = 10 * 1024 * 1024);
+    Buddha(const Params & p, const std::size_t thread_vector_size = 10 * 1024 * 1024);
 
-	static Params get_empty_params();
+    static Params get_empty_params();
 
-	void run();
+    void run();
 private:
-	uint64_t x_size_;
-	uint64_t y_size_;
+    uint64_t x_size_;
+    uint64_t y_size_;
     floating_type radius_;
     uint64_t max_iterations_;
 
-	std::vector<uint64_t> data_;
-	std::mutex data_lock_;
+    std::vector<uint64_t> data_;
+    std::mutex data_lock_;
 
-	std::pair<uint64_t, uint64_t> lin2car(uint64_t pos) const;
-	uint64_t car2lin(uint64_t x, uint64_t y) const;
+    std::pair<uint64_t, uint64_t> lin2car(uint64_t pos) const;
+    uint64_t car2lin(uint64_t x, uint64_t y) const;
 
-	complex_type car2complex(uint64_t x, uint64_t y) const;
-	std::pair<uint64_t, uint64_t> complex2car(complex_type c) const;
+    complex_type car2complex(uint64_t x, uint64_t y) const;
+    std::pair<uint64_t, uint64_t> complex2car(complex_type c) const;
 
     uint64_t complex2lin(complex_type c) const;
 
-	const std::size_t thread_vector_size_;
-	std::size_t num_threads_;
-	std::vector<std::thread> threads_;
-	void worker(uint64_t from, uint64_t to);
+    const std::size_t thread_vector_size_;
+    std::size_t num_threads_;
+    std::vector<std::thread> threads_;
+    void worker(uint64_t from, uint64_t to);
 };
 
 #endif // _BUDDHA_H
